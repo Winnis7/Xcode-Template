191bf99a658cdecbc6e3144e6b73ba5df6a5d611
Winnis <winnisx7@gmail.com>
2022-06-29 16:12:45 +0900

    1.0.0

---
+++ Base View.xctemplate/BaseViewXIB/___FILEBASENAME___.swift
@@ -0,0 +1,57 @@
+//___FILEHEADER___
+
+import UIKit
+import ReactorKit
+import RxSwift
+import RxCocoa
+
+// MARK: - View
+class ___VARIABLE_productName___: BaseXibView, StoryboardView {
+  // MARK: Abstract
+  typealias Reactor = ___VARIABLE_productName___Reactor
+
+  // MARK: UI Component
+
+  // MARK: Properties
+
+  // MARK: Initialization
+  override func commonInit() {
+    super.commonInit()
+
+    // ContentView injection required at this point.
+    self.contentView = ___VARIABLE_productName___.loadFromNib()
+  }
+
+  // MARK: View Life Cycle
+  override func awakeFromNib() {
+    super.awakeFromNib()
+
+  }
+
+  // MARK: Rx Binding
+  func bind(reactor: Reactor) {
+    // MARK: State
+    
+    // MARK: View
+    
+  }
+}
+
+// MARK: - Reactor
+class ___VARIABLE_productName___Reactor: Reactor {
+  // MARK: Abstract
+  typealias Action = NoAction
+
+  struct State {
+
+  }
+
+  // MARK: Properties
+  let scheduler: Scheduler = MainScheduler.instance
+  let initialState: State
+
+  // MARK: Initialization
+  init() {
+    initialState = State()
+  }
+}
